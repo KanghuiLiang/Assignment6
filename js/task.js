dfd660d4ad95cde53c91380372d43dc1d33b866e
liangkanghui <liangkanghuiapu@gmail.com>
2017-12-03 19:15:45 +0700

    Added

---
+++ js/task.js
@@ -0,0 +1,50 @@
+var data = "<p>width: " + window.innerWidth + "</P>";
+data += "<p>height: " + window.innerHeight + "</p>";
+data +="<p>Current location: " +window.location + "</p>";
+
+var ele = document.getElementById('data');
+ele.innerHTML =data;
+
+function load() {
+	location.reload();
+	// var load = document.getElementById('button');
+	// load.innerHTML = myFunction();
+}
+
+function changeColor() {
+	var col = document.getElementById("color");
+	col.style.color = "yellow";
+}
+
+function append() {
+    var app = document.getElementById("append");
+    var text = document.createTextNode("Hello World "); 
+    app.appendChild(text);  
+}
+
+function hideOrShow() {
+	var e = document.getElementById("Hide-show");
+	if (e.style.visibility == "hidden") {
+		e.style.visibility = "visible";
+	} else {
+		e.style.visibility = "hidden";
+	}
+}
+
+function mouseOver(obj) {
+	obj.innerHTML = "mouse over"
+}
+function mouseOut(obj) {
+	obj.innerHTML = "mouse out"
+}
+function mouseUp(obj) {
+	obj.innerHTML = "mouse up"
+}
+function mouseDown(obj) {
+	obj.innerHTML = "mouse down"
+}
+
+function doubleClick(obj) {
+	obj.innerHTML = "Double click"
+}
+
